5be931cc5f2b8965fa1e6598e5cd160bac35fe1a
Sandie Nunez <snunez@vt.edu>
2020-04-05 04:06:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,12 +3,12 @@ def my_hash
 end
 
 
-def shipping_manifest
+
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+def shipping_manifest
+the_manifest
 end
 
 def retrieval
